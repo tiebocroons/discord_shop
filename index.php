df470a4a34c69e98ff8aca5058483f309025dedd
HyphaSlayer <tiebo.croons@gmail.com>
2024-10-09 09:54:08 +0200

    feat: added index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+    header("Location: login.php");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Welkom</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="login-container">
+        <div class="login-box">
+            <h1>Welkom, <?php echo $_SESSION['username']; ?>!</h1>
+            <p>Je bent succesvol ingelogd.</p>
+            <form action="logout.php" method="POST">
+                <button type="submit">Logout</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
