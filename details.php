a112595b50861f1fa54a50eb20a6320dd74862d2
HyphaSlayer <tiebo.croons@gmail.com>
2024-12-08 15:35:10 +0100

    feat: update stylesheet link to use details.css for improved styling on product details page

--- details.php
+++ details.php
@@ -41,7 +41,7 @@ $reviews = $reviewManager->fetchReviews($productId);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Product Details</title>
     <link rel="stylesheet" href="css/general.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/details.css">
 </head>
 <body>
     <div class="container">
