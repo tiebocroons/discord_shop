fb127304320f2315a1ae0af9eab156ce40cc3f12
HyphaSlayer <tiebo.croons@gmail.com>
2024-12-08 17:07:37 +0100

    fix: update require paths in add_comment.php to use correct relative directory structure

--- ajax/add_comment.php
+++ ajax/add_comment.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-require_once __DIR__ . "/classes/Database.php";
-require_once __DIR__ . "/classes/Comment.php";
+require_once __DIR__ . "/../classes/Database.php";
+require_once __DIR__ . "/../classes/Comment.php";
 
 header('Content-Type: application/json');
 
